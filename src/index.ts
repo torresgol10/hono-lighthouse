e82e65b6a9becf8c6dee16c1f89b39800dbc0b4b
Ivan Torres <40922354+torresgol10@users.noreply.github.com>
2025-04-02 19:35:00 +0200

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,49 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { logger } from 'hono/logger'
+
+import fs from "fs";
+import lighthouse from "lighthouse";
+import * as chromeLauncher from "chrome-launcher";
+
+const app = new Hono();
+app.use(logger())
+
+app.get("/", async (c) => {
+  const chrome = await chromeLauncher.launch({ chromeFlags: ["--headless"] });
+  const options = {
+    //logLevel: "info",
+    output: "json",
+    onlyCategories: ["performance"],
+    port: chrome.port,
+  };
+  const runnerResult = await lighthouse("https://www.fotocasa.es/es/comprar/vivienda/estepona/terraza-ascensor-piscina-piscina/186023639/d?from=list", options);
+
+  // `.report` is the HTML report as a string
+  fs.writeFileSync("new-path.webp", runnerResult.artifacts.FullPageScreenshot.screenshot.data.replace(/^data:image\/webp;base64,/, ''), 'base64');
+
+  const reportHtml = runnerResult.report;
+  fs.writeFileSync("lhreport2.html", reportHtml);
+
+  // `.lhr` is the Lighthouse Result as a JS object
+  console.log("Report is done for", runnerResult.lhr.finalDisplayedUrl);
+  console.log(
+    "Performance score was",
+    runnerResult.lhr.categories.performance.score * 100
+  );
+
+  chrome.kill();
+
+  //return c.html(runnerResult.report);
+  return c.text("ok");
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
